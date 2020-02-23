f7b61e015c026cb3d7892c522d680dc1f7afa586
kehochman <58151660+kehochman@users.noreply.github.com>
2020-02-23 14:35:11 -0500

    first push

---
+++ queries.sql
@@ -0,0 +1,11 @@
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+
+SELECT 
+    e.emp_no,
+    e.last_name,
+    e.first_name,
+    e.gender,
+    s.salary
+FROM employees e 
+JOIN salaries s 
+USING (emp_no)
